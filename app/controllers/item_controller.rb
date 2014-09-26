bb7ffb452ac06b8a50f1e042b1f63e77d182648b
esodov <esodov+github@gmail.com>
2014-09-26 06:32:12 +0000

    itemController added.

---
+++ app/controllers/item_controller.rb
@@ -0,0 +1,25 @@
+class ItemController < ApplicationController
+  def search
+    httpClient = HTTPClient.new
+
+    //楽天WebAPIにリクエストを投げて、結果を取得する
+    begin
+      data = httpClient.get_content('https://app.rakuten.co.jp/services/api/Product/Search/20140305', {
+        'applicationId' => ENV["APPID"],
+        'affiliateId' => ENV["AFID"],
+        'keyword' => keyword
+      })
+    rescue HTTPClient::BadResponseError => e
+    rescue HTTPClient::TimeoutError => e
+
+    //JsonをRubyで扱いやすいようにパースする
+    jsonData = JSON.parse(data)
+
+    @products = Array.New
+    jsonData['product'].each{|json_procuct||
+      products.push(product.New(json_product)
+    }
+
+    render 'welcome/index'
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
+require 'json'
+
 class Product < ActiveRecord::Base
+  def initialize(jsonData))
+    //変数に代入
+    @name = jsonData['name']
+    @postage = jsonData['postage']
+    @imageUrl = jsonData[['imageUrl']
+    @salesMinPrice = jsonData['salesMinPrice']
+    @usedExcludeMinPrice = jsonData['usedExcludeMinPrice ']
+    @usedExcludeSalesMinPrice = jsonData['usedExcludeSalesMinPrice']
+    @minPrice = jsonData['minPrice']
+  end
 end
