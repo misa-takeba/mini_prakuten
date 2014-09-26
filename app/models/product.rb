1a740c69102a37b9069976adee65f2e52dcb6708
Yoshihide Chubachi <yoshi@chubachi.net>
2014-09-26 07:50:24 +0000

    Fix syntax error

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 require 'json'
 
 class Product < ActiveRecord::Base
-  def initialize(jsonData))
-    //変数に代入
+  def initialize(jsonData)
+    # 変数に代入
     @name = jsonData['name']
     @postage = jsonData['postage']
-    @imageUrl = jsonData[['imageUrl']
+    @imageUrl = jsonData['imageUrl']
     @salesMinPrice = jsonData['salesMinPrice']
     @usedExcludeMinPrice = jsonData['usedExcludeMinPrice ']
     @usedExcludeSalesMinPrice = jsonData['usedExcludeSalesMinPrice']
